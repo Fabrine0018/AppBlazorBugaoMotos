c14d9e062b0d13901f6f143fadbcb65a5146d81b
Fabrine0018 <132179677+Fabrine0018@users.noreply.github.com>
2025-09-30 16:39:14 -0400

    Model Fornecedor

---
+++ Components/Models/Fornecedor.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace AppBugaoMotoFVLE.Components.Models
+{
+    public class Fornecedor
+    {
+        public string? Responsavel { get; set; }
+        public string CEP { get; set; }
+        public string CNPJ { get; set; }
+        public string TelefoneRes { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public int NumeroCasaApt { get; set; }
+        public string Complemento { get; set; }
+        public string RazaoSocial { get; set; }
+        public string TelefoneEmp { get; set; }
+        
+        /* endereço a decidir se será normalizado */
+    }
+}
---
+++ Components/Models/FornecedorDAO.cs
@@ -0,0 +1,36 @@
+﻿namespace AppBugaoMotoFVLE.Components.Models
+{
+    public class FornecedorDAO
+    {
+        //private readonly Conexao _conexao;
+        //public FornecedorDAO (Conexao conexao)
+        //{
+        //_conexao = conexao;
+        //}
+
+        //public void InserirFornecedor(Produto produto)
+        //{
+        //    try
+        //    {
+        //        var comando = _conexao.CreateCommand("INSERT INTO fornecedor Values (null, null, @...)");
+        //        comando.Parameters.AddWithValue("@_", produto.Responsavel);
+        //        comando.Parameters.AddWithValue("@_", produto.CEP);
+        //        comando.Parameters.AddWithValue("@_", produto.CNPJ);
+        //        comando.Parameters.AddWithValue("@_", produto.TelefoneRes);
+        //        comando.Parameters.AddWithValue("@_", produto.Rua);
+        //        comando.Parameters.AddWithValue("@_", produto.Bairro);
+        //        comando.Parameters.AddWithValue("@_", produto.Cidade);
+                  //comando.Parameters.AddWithValue("@_", produto.Estado);
+                  //comando.Parameters.AddWithValue("@_", produto.NumeroCasaApt);
+                  //comando.Parameters.AddWithValue("@_", produto.Complemento);
+                  //comando.Parameters.AddWithValue("@_", produto.RazaoSocial);
+                  //comando.Parameters.AddWithValue("@_", produto.TelefoneEmp);
+                 // comando.ExecuteNonQuery();
+
+        //    } catch (Exception)
+        //    {
+                //throw;
+        //     }
+        //}
+    }
+}
