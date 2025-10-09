8f3eddf687749acd28038b7243c375c9f42af8a7
PanquecaFrita <vivianeguimaraes080915@gmail.com>
2025-10-09 09:13:45 -0400

    Atualizações

--- Components/Models/Cliente.cs
+++ Components/Models/Cliente.cs
@@ -9,7 +9,7 @@
     public string? Estado { get; set; }
     public string? Cpf { get; set; }
     public string? Cidade { get; set; }
-    public string? Completo { get; set; }
+    public string? Complemento { get; set; }
     public string? Bairro { get; set; }
     public string? Rua { get; set; }
     public string? Cep { get; set; }
--- Components/Models/ClienteDAO.cs
+++ Components/Models/ClienteDAO.cs
@@ -17,13 +17,13 @@ public class ClienteDAO
     {
         try
         {
-            var comando = _conexao.CreateCommand("INSERT INTO fornecedor VALUES (null, @_nome_clie, @_telefone_clie, @_cep_clie, @_complemento_clie, @_cpf_clie, @_rua_clie, @_estado_clie, @_cidade_clie, @_bairro_clie)");
+            var comando = _conexao.CreateCommand("INSERT INTO Cliente VALUES (null, @_nome_clie, @_telefone_clie, @_cep_clie, @_complemento_clie, @_cpf_clie, @_rua_clie, @_estado_clie, @_cidade_clie, @_bairro_clie)");
             comando.Parameters.AddWithValue("@_nome_clie", cliente.Nome);
             comando.Parameters.AddWithValue("@_telefone_clie",cliente.Telefone);
      
        
             comando.Parameters.AddWithValue("@_cep_clie", cliente.Cep);
-            comando.Parameters.AddWithValue("@_complemento_clie", cliente.Completo);
+            comando.Parameters.AddWithValue("@_complemento_clie", cliente.Complemento);
             comando.Parameters.AddWithValue("@_cpf_clie", cliente.Cpf);
            
             comando.Parameters.AddWithValue("@_rua_clie", cliente.Rua);
@@ -38,32 +38,32 @@ public class ClienteDAO
             throw;
         }
     }
-
-    public List<Cliente> ListarTodoClie()
+    //Eduardo arrumou
+    public List<Cliente> ListarCliente()
     {
         var listaClie = new List<Cliente>();
 
-        var comando = _conexao.CreateCommand("SELECT * FROM produto;");
+        var comando = _conexao.CreateCommand("SELECT * FROM Cliente;");
         var leitor = comando.ExecuteReader();
 
         while (leitor.Read())
         {
             //ERRO AQUI
-            //var cliente = new Cliente();
-            //cliente.IdCli = leitor.GetInt32("id_clie");
-            //cliente.Nome_Clie = DAOHelper.GetString(leitor, "nome_clie");
-            //cliente.Telefone_Clie = DAOHelper.GetString(leitor, "telefone_clie");
-            //cliente.Estado_Clie = DAOHelper.GetString(leitor, "estado_clie");
-            //cliente.Cpf_Clie = DAOHelper.GetString(leitor, "cpf_clie");
-            //cliente.Cidade_Clie = DAOHelper.GetString(leitor, "cidade_clie");
-            //cliente.Completo_Clie = DAOHelper.GetString(leitor, "complemento_clie");
-            //cliente.Bairro_Clie = DAOHelper.GetString(leitor, "bairro_clie");
-            //cliente.Rua_Clie = DAOHelper.GetString(leitor, "rua_clie");
-            //cliente.Cep_clie = DAOHelper.GetString(leitor, "cep_clie");
+            var cliente = new Cliente();
+            cliente.IdCli = leitor.GetInt32("id_clie");
+            cliente.Nome = DAOHelper.GetString(leitor, "nome_clie");
+            cliente.Telefone = DAOHelper.GetString(leitor, "telefone_clie");
+            cliente.Estado = DAOHelper.GetString(leitor, "estado_clie");
+            cliente.Cpf = DAOHelper.GetString(leitor, "cpf_clie");
+            cliente.Cidade = DAOHelper.GetString(leitor, "cidade_clie");
+            cliente.Complemento = DAOHelper.GetString(leitor, "complemento_clie");
+            cliente.Bairro = DAOHelper.GetString(leitor, "bairro_clie");
+            cliente.Rua = DAOHelper.GetString(leitor, "rua_clie");
+            cliente.Cep = DAOHelper.GetString(leitor, "cep_clie");
 
 
 
-            //listaClie.Add(cliente);
+            listaClie.Add(cliente);
         }
 
         return listaClie;
